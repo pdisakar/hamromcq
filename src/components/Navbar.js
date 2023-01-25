071de43d202dfadb023ec8c499d2b7f139574f5f
Sakar <barcaboysakar@gmail.com>
2023-01-25 09:22:47 +0545

    Doing some changes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,17 +3,17 @@ import { Link } from 'react-router-dom';
 export default function Navbar() {
   return (
     <div>
-      <nav class="bg-white border-blue-200 px-2 sm:px-4 py-2.5 dark:bg-blue-900">
-        <div class="container flex flex-wrap items-center justify-between mx-auto">
+      <nav className="bg-white border-blue-200 px-2 sm:px-4 py-2.5 dark:bg-blue-900">
+        <div className="container flex flex-wrap items-center justify-between mx-auto">
           {/* <a
             href="/"
-            class="flex items-center">
+            className="flex items-center">
             <img
               src="favicon.ico"
-              class="h-6 mr-3 sm:h-9"
+              className="h-6 mr-3 sm:h-9"
               alt="Flowbite Logo"
             />
-            <span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white">
+            <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">
               Hamro MCQ
             </span>
           </a> */}
@@ -22,27 +22,28 @@ export default function Navbar() {
             className="flex items-center">
             <img
               src="favicon.ico"
-              class="h-6 mr-3 sm:h-9"
+              className="h-6 mr-3 sm:h-9"
               alt="Flowbite Logo"
             />
-            <span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white">
+            <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">
               Hamro MCQ
             </span>
           </Link>
-          <div class="flex md:order-2">
+          <div className="flex md:order-2">
             <button
               type="button"
-              class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-3 md:mr-0 bg-blue-600 hover:bg-blue-700 focus:ring-blue-800">
+              className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-3 md:mr-0 bg-blue-600 hover:bg-blue-700 focus:ring-blue-800">
               Get started
             </button>
+
             <button
               data-collapse-toggle="navbar-cta"
               type="button"
-              class="inline-flex items-center p-2 text-sm text-blue-500 rounded-lg md:hidden hover:bg-blue-100 focus:outline-none focus:ring-2 focus:ring-blue-200 text-white hover:bg-blue-700 focus:ring-blue-600"
+              className="inline-flex items-center p-2 text-sm text-blue-500 rounded-lg md:hidden hover:bg-blue-100 focus:outline-none focus:ring-2 focus:ring-blue-200 text-white hover:bg-blue-700 focus:ring-blue-600"
               aria-controls="navbar-cta"
               aria-expanded="false">
               <svg
-                class="w-6 h-6 "
+                className="w-6 h-6 "
                 aria-hidden="true"
                 fill="currentColor"
                 viewBox="0 0 20 20"
@@ -55,13 +56,13 @@ export default function Navbar() {
             </button>
           </div>
           <div
-            class="items-center justify-between hidden w-full md:flex md:w-auto md:order-1"
+            className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1"
             id="navbar-cta">
-            <ul class="flex flex-col p-4 mt-4 border border-blue-100 rounded-lg bg-blue-50 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-white dark:bg-blue-800 md:dark:bg-blue-900 dark:border-blue-700">
+            <ul className="flex flex-col p-4 mt-4 border border-blue-100 rounded-lg bg-blue-50 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-white dark:bg-blue-800 md:dark:bg-blue-900 dark:border-blue-700">
               <li>
                 {/* <a
                   href="/question-bank"
-                  class="block py-2 pl-3 pr-4 text-white rounded md:hover:bg-transparent md:p-0 dark:hover:bg-blue-700 hover:underline">
+                  className="block py-2 pl-3 pr-4 text-white rounded md:hover:bg-transparent md:p-0 dark:hover:bg-blue-700 hover:underline">
                   Questions Banks
                 </a> */}
                 <Link
@@ -74,7 +75,7 @@ export default function Navbar() {
               <li>
                 {/* <a
                   href="#"
-                  class="block py-2 pl-3 pr-4 text-white rounded md:hover:bg-transparent md:p-0 dark:hover:bg-blue-700 hover:underline">
+                  className="block py-2 pl-3 pr-4 text-white rounded md:hover:bg-transparent md:p-0 dark:hover:bg-blue-700 hover:underline">
                   Exams
                 </a> */}
                 <Link
@@ -86,7 +87,7 @@ export default function Navbar() {
               <li>
                 {/* <a
                   href="#"
-                  class="block py-2 pl-3 pr-4 text-white rounded md:hover:bg-transparent md:p-0 dark:hover:bg-blue-700 hover:underline">
+                  className="block py-2 pl-3 pr-4 text-white rounded md:hover:bg-transparent md:p-0 dark:hover:bg-blue-700 hover:underline">
                   Halls
                 </a> */}
                 <Link
@@ -98,7 +99,7 @@ export default function Navbar() {
               <li>
                 {/* <a
                   href="#"
-                  class="block py-2 pl-3 pr-4 text-white rounded md:hover:bg-transparent md:p-0 dark:hover:bg-blue-700 hover:underline">
+                  className="block py-2 pl-3 pr-4 text-white rounded md:hover:bg-transparent md:p-0 dark:hover:bg-blue-700 hover:underline">
                   Contact Us
                 </a> */}
                 <Link
