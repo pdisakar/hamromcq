1e3a19322daa6fca343b0afa6cdb74050a9bfea8
Sakar <barcaboysakar@gmail.com>
2023-01-25 09:45:02 +0545

    Doing some changes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -32,14 +32,14 @@ export default function Navbar() {
           <div className="flex md:order-2">
             <button
               type="button"
-              className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-3 md:mr-0 bg-blue-600 hover:bg-blue-700 focus:ring-blue-800">
+              className="text-white focus:ring-4 focus:outline-none  font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-3 md:mr-0 bg-blue-600 hover:bg-blue-700 focus:ring-blue-800">
               Get started
             </button>
 
             <button
               data-collapse-toggle="navbar-cta"
               type="button"
-              className="inline-flex items-center p-2 text-sm text-blue-500 rounded-lg md:hidden hover:bg-blue-100 focus:outline-none focus:ring-2 focus:ring-blue-200 text-white hover:bg-blue-700 focus:ring-blue-600"
+              className="inline-flex items-center p-2 text-sm rounded-lg md:hidden focus:outline-none focus:ring-2 focus:ring-blue-200 text-white hover:bg-blue-700 "
               aria-controls="navbar-cta"
               aria-expanded="false">
               <svg
