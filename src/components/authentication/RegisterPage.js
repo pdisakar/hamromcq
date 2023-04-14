c7bc4be71ad82bca341ac3251572980ad56cda49
Sakar Pudasaini <barcaboysakar@gmil.com>
2023-04-14 19:23:17 +0545

    removing firebase

--- src/components/authentication/RegisterPage.js
+++ src/components/authentication/RegisterPage.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 export default function RegisterPage() {
-  return (
+    return (
     <div>
       <section className="bg-gray-50 dark:bg-gray-900 h-screen">
         <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
@@ -38,6 +38,7 @@ export default function RegisterPage() {
                     className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                     placeholder="This is Fear"
                     required=""
+                   
                   />
                 </div>
                 <div>
@@ -53,6 +54,7 @@ export default function RegisterPage() {
                     className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                     placeholder="you@exmple.com"
                     required=""
+                    
                   />
                 </div>
                 <div>
@@ -68,6 +70,7 @@ export default function RegisterPage() {
                     placeholder="••••••••"
                     className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                     required=""
+                  
                   />
                 </div>
 
@@ -95,7 +98,7 @@ export default function RegisterPage() {
                   </div>
                 </div>
                 <button
-                  type="submit"
+                 
                   className="w-full text-white bg-primary-600 hover:bg-primary-700 ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">
                   Create an account
                 </button>
@@ -115,3 +118,4 @@ export default function RegisterPage() {
     </div>
   );
 }
+ 
---
+++ src/components/indexpages/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function index() {
+  return (
+    <div>
+      <p>User Email: </p>
+      <button className='border px-6 py-2 my-4'>Logout</button>
+    </div>
+  );
+}
